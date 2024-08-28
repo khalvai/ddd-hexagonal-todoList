de5f3f6c6b322dbbf7943255ee70b6e586df441e
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-28 11:57:13 +0330

    feat: dto for login and register for swagger is added.

--- src/User/Infrastructure/Input/HTTP/Dto/LoginDTO.ts
+++ src/User/Infrastructure/Input/HTTP/Dto/LoginDTO.ts
@@ -1,18 +1,16 @@
-import { LoginQuery } from "src/User/Application/Queries/LoginQuery";
-import { ApiProperty } from "@nestjs/swagger";
+import { LoginQuery } from 'src/User/Application/UseCases/Queries/Login/LoginQuery';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDTO implements LoginQuery {
-
-
-    @ApiProperty({
-        required: true,
-        example: "Khalvayozbek@gmail.com"
-    })
-    email: string;
-    @ApiProperty({
-        required: true,
-        example: "SOME_STRONG_PASSWORD",
-        description: "length must be between 8 and 20 characters"
-    })
-    password: string;
+  @ApiProperty({
+    required: true,
+    example: 'Khalvayozbek@gmail.com',
+  })
+  email: string;
+  @ApiProperty({
+    required: true,
+    example: 'SOME_STRONG_PASSWORD',
+    description: 'length must be between 8 and 20 characters',
+  })
+  password: string;
 }
--- src/User/Infrastructure/Input/HTTP/Dto/ReginsterDTO.ts
+++ src/User/Infrastructure/Input/HTTP/Dto/ReginsterDTO.ts
@@ -1,33 +1,28 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { RegisterCommand } from "src/User/Application/Commands/RegisterCommand";
-
+import { ApiProperty } from '@nestjs/swagger';
+import { RegisterCommand } from 'src/User/Application/UseCases/Commands/Register/RegisterCommand';
 
 export class RegisterDTO implements RegisterCommand {
-    ip: string;
+  ip: string;
 
-    @ApiProperty({
-        required: true,
-        example: "Khalvayozbek@gmail.com"
-    })
-    email: string;
-    @ApiProperty({
-        required: true,
-        example: "SOME_STRONG_PASSWORD",
-        description: "length must be between 8 and 20 characters"
-    })
-    password: string;
-    @ApiProperty({
-        required: true,
-        example: "SOME_STRONG_PASSWORD",
-
-    })
-    confirmPassword: string;
-    @ApiProperty({
-        required: true,
-        example: "khalvai"
-
-    })
-    name: string;
+  @ApiProperty({
+    required: true,
+    example: 'Khalvayozbek@gmail.com',
+  })
+  email: string;
+  @ApiProperty({
+    required: true,
+    example: 'SOME_STRONG_PASSWORD',
+    description: 'length must be between 8 and 20 characters',
+  })
+  password: string;
+  @ApiProperty({
+    required: true,
+    example: 'SOME_STRONG_PASSWORD',
+  })
+  confirmPassword: string;
+  @ApiProperty({
+    required: true,
+    example: 'khalvai',
+  })
+  name: string;
 }
-
-
