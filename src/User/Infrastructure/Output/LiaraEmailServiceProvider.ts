493fe1525d3333fec712ff3b65e0b8d13a6f4532
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-28 12:07:24 +0330

    fix: changed the import path from application.

--- src/User/Infrastructure/Output/LiaraEmailServiceProvider.ts
+++ src/User/Infrastructure/Output/LiaraEmailServiceProvider.ts
@@ -1,13 +1,11 @@
-import Result from "src/Common/Application/Result";
-import { EmailServiceProvider } from "src/User/Application/Ports/Output/EmailServiceProvider";
-import Email from "src/User/Domain/Email";
-
+import Result from 'src/Common/Application/Result';
+import { EmailServiceProvider } from 'src/User/Application/Ports/EmailServiceProvider';
+import Email from 'src/User/Domain/Email';
 
 export class LiaraEmailServiceProvider implements EmailServiceProvider {
-    async send(template: string, to: Email): Promise<Result<void>> {
+  async send(template: string, to: Email): Promise<Result<void>> {
+    // talk to a 3rd party to send email
 
-        // talk to a 3rd party to send email
-
-        return { ok: undefined };
-    }
+    return { ok: undefined };
+  }
 }
--- src/User/Infrastructure/Output/Template/EjsTemplate.ts
+++ src/User/Infrastructure/Output/Template/EjsTemplate.ts
@@ -1,22 +1,19 @@
-import { Template } from "src/User/Application/Ports/Output/Template";
-
+import { Template } from 'src/User/Application/Ports/Template';
 
 export class EJSTemplate implements Template {
-
-
-    getWelcomeTemplate(userFirstName: string, userLastName: string): string {
-        return `some ejs template that uses ${userFirstName} ${userLastName}`;
-    }
-    getVerifyEMailTemplate(token: string, email: string, ip: string): string {
-        return `some ejs file that uses ${token}, ${email} and ${ip}`;
-    }
-    getChangingEmailTemplate(token: string, mail: string, ip: string): string {
-        throw new Error("Method not implemented.");
-    }
-    getForgotPasswordTemplate(token: string, mail: string, ip: string): string {
-        throw new Error("Method not implemented.");
-    }
-    getYouHaveChangeEmailTemplate(latterMail: string, ip: string, changedDate: Date): string {
-        throw new Error("Method not implemented.");
-    }
+  getWelcomeTemplate(userFirstName: string, userLastName: string): string {
+    return `some ejs template that uses ${userFirstName} ${userLastName}`;
+  }
+  getVerifyEMailTemplate(token: string, email: string, ip: string): string {
+    return `some ejs file that uses ${token}, ${email} and ${ip}`;
+  }
+  getChangingEmailTemplate(token: string, mail: string, ip: string): string {
+    throw new Error('Method not implemented.');
+  }
+  getForgotPasswordTemplate(token: string, mail: string, ip: string): string {
+    throw new Error('Method not implemented.');
+  }
+  getYouHaveChangeEmailTemplate(latterMail: string, ip: string, changedDate: Date): string {
+    throw new Error('Method not implemented.');
+  }
 }
