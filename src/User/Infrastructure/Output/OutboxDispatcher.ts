225d47a7b250f1e0bfe5074c7df2e5d9f7a87ab3
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-28 11:58:00 +0330

    feat: make dispatched event mark them as  it is.

--- src/User/Infrastructure/Output/OutboxDispatcher.ts
+++ src/User/Infrastructure/Output/OutboxDispatcher.ts
@@ -1,31 +1,30 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { Cron } from "@nestjs/schedule";
-import { Publisher } from "src/Common/Application/Output/Publisher";
-import { OutboxRepository } from "src/User/Application/Ports/Output/OutboxRepository";
+import { Inject, Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { Publisher } from 'src/Common/Application/Output/Publisher';
+import { OutboxRepository } from 'src/User/Application/Ports/OutboxRepository';
 
-// Suggestion: this code has n+1 problem we can optimize it 
+// Suggestion: this code has n+1 problem we can optimize it
 @Injectable()
 export class OutboxDispatcher {
   constructor(
     @Inject(OutboxRepository)
     private readonly outboxRepository: OutboxRepository,
     @Inject(Publisher) private readonly publisher: Publisher,
-  ) { }
+  ) {}
 
-  @Cron("*/20 * * * * *")
+  @Cron('*/20 * * * * *')
   async dispatchEvents() {
     console.log(`cron is running: ${Date.now()}`);
 
     try {
       const outboxes = await this.outboxRepository.getUnDispatched();
 
-
       for (const outbox of outboxes) {
         await this.publisher.publish(outbox.name, outbox);
-        // await this.outboxRepository.dispatched(outbox.id);
+        await this.outboxRepository.dispatched(outbox.id);
       }
     } catch (error) {
-      console.error("Failed to dispatch event", error);
+      console.error('Failed to dispatch event', error);
     }
   }
 }
