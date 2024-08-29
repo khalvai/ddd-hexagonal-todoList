3f26ae8b9e2647152d1f7c12dbfe80724f16da4d
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-29 05:31:06 +0330

    fix: removed unnecessary document models.

--- src/TodoList/Infrastructure/Output/MongoDB/MongodbModule.ts
+++ src/TodoList/Infrastructure/Output/MongoDB/MongodbModule.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TodoListDocument, TodoListSchema } from 'src/TodoList/Infrastructure/Output/MongoDB/TodoListSchema';
+import { ItemDocument, ItemsSchema, TodoListDocument, TodoListSchema } from 'src/TodoList/Infrastructure/Output/MongoDB/TodoListSchema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: TodoListDocument.name, schema: TodoListSchema }])],
