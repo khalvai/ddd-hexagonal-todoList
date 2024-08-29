29d312c55939c9ad90d28fa5e9703221dcc6b6a7
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-29 13:13:48 +0330

    docs: swagger title changed to Todo List.

--- src/Common/Infrastructure/Input/Swagger.ts
+++ src/Common/Infrastructure/Input/Swagger.ts
@@ -4,27 +4,22 @@ import { getThemeAsync } from '@intelika/swagger-theme';
 import simpleGit from 'simple-git';
 
 export async function setupDocument(app: INestApplication, route: string) {
+  const description = [`🍕  documentation`];
 
+  const configDocument = new DocumentBuilder()
+    .setTitle('Todo List')
+    .setDescription(description.join('\r\n\r\n'))
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'Bearer',
+      bearerFormat: 'JWT',
+    })
+    .build();
 
-    const description = [`🍕  documentation`];
-
-
-    const configDocument = new DocumentBuilder()
-        .setTitle('assignmentl')
-        .setDescription(description.join('\r\n\r\n'))
-        .setVersion('1.0')
-        .addBearerAuth({
-            type: 'http',
-            scheme: 'Bearer',
-            bearerFormat: 'JWT'
-        })
-        .build();
-
-    const document = SwaggerModule.createDocument(app, configDocument);
-    const style: Buffer = await getThemeAsync();
-    SwaggerModule.setup(route, app, document, {
-        customCss: style.toString()
-    });
-
-
+  const document = SwaggerModule.createDocument(app, configDocument);
+  const style: Buffer = await getThemeAsync();
+  SwaggerModule.setup(route, app, document, {
+    customCss: style.toString(),
+  });
 }
