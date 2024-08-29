7a15b58f50681c162279cd4791b348f8f869f0a6
khalvai <78291644+khalvai@users.noreply.github.com>
2024-08-29 05:32:18 +0330

    feat: to read model method is implemented.

--- src/TodoList/Infrastructure/Output/Mappers/TodoListMapper.ts
+++ src/TodoList/Infrastructure/Output/Mappers/TodoListMapper.ts
@@ -1,3 +1,4 @@
+import { ItemReadModel, TodoListReadModel } from 'src/TodoList/Application/UseCases/Queries/TodoListReadModel';
 import { Description } from 'src/TodoList/Domain/Item/Description';
 import { Item } from 'src/TodoList/Domain/Item/Item';
 import { ItemId } from 'src/TodoList/Domain/Item/ItemId';
@@ -10,12 +11,12 @@ import { TodoListPersistenceModel } from 'src/TodoList/Infrastructure/Output/Mod
 export class TodoListMapper {
   static toDomain(model: TodoListPersistenceModel): TodoList {
     const todoList = new TodoList();
-    todoList.id = TodoListId.fromValid(model.title);
+    todoList.id = TodoListId.fromValid(model.id);
     todoList.title = Title.fromValid(model.title);
     todoList.concurrencySafeVersion = model.concurrencySafeVersion;
     todoList.createdAt = model.createdAt;
     todoList.updatedAt = model.updatedAt;
-
+    todoList.items = [];
     for (const itemModel of model.items) {
       const item = new Item();
       item.id = ItemId.fromValid(itemModel.id);
@@ -30,5 +31,43 @@ export class TodoListMapper {
     }
 
     return todoList;
+  }
+
+  static toReadModel(todoList: TodoList): TodoListReadModel {
+    const model: TodoListReadModel = {
+      id: todoList.id.value,
+      title: todoList.title.value,
+      createdAt: todoList.createdAt,
+      updatedAt: todoList.updatedAt,
+      items: [],
+    };
+
+    const uItem: ItemReadModel[] = [];
+    const hItem: ItemReadModel[] = [];
+    const mItem: ItemReadModel[] = [];
+    const lItem: ItemReadModel[] = [];
+
+    for (const item of todoList.items) {
+      const itemReadModel: ItemReadModel = {
+        id: item.id.value,
+        title: item.title.value,
+        description: item.description.value,
+        priority: item.priority.value,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt,
+      };
+
+      if (item.priority.value === Priority.Urgent) uItem.push(itemReadModel);
+      if (item.priority.value === Priority.High) hItem.push(itemReadModel);
+      if (item.priority.value === Priority.Medium) mItem.push(itemReadModel);
+      if (item.priority.value === Priority.Low) lItem.push(itemReadModel);
+    }
+
+    model.items.push(...uItem);
+    model.items.push(...hItem);
+    model.items.push(...mItem);
+    model.items.push(...lItem);
+
+    return model;
   }
 }
